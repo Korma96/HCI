7e97fb30a50e790f224f9d88e8aad6af2d298c85
Jovo <sunjkajovo@gmail.com>
2018-04-15 15:46:34 +0200

    Omogucen rad sa crypto valutama i obicnim valutama

--- HCI/ViewModel/Controller.cs
+++ HCI/ViewModel/Controller.cs
@@ -1,10 +1,13 @@
-﻿using Newtonsoft.Json;
+﻿using HCI.View;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,46 +16,106 @@ namespace HCI.ViewModel
 
     public class Controller
     {
+        private WaitDialog waitDialog;
 
-        private Dictionary<string, string> symbols;
+        private Dictionary<string, string> shares;
+        private Dictionary<string, string> cryptos;
+        private Dictionary<string, string> currs;
 
-        public Controller()
+        string sharesPath = @"..\..\Files\shares.csv";
+        string cryptoPath = @"..\..\Files\crypto.csv";
+        string currPath = @"..\..\Files\curr.csv";
+
+    public Controller()
         {
-            readSymbolsOfCompanies();
+            shares = readFromFile(sharesPath);
+            cryptos = readFromFile(cryptoPath);
+            currs = readFromFile(currPath);
         }
 
-        public DataFromNetwork downloadSerializedJsonData(string url)
+        public string downloadJsonData(string url)
         {
-            using (WebClient w = new WebClient())
+            string jsonData = string.Empty;
+
+            using (WebClient wc = new WebClient())
             {
-                string jsonData = string.Empty;
                 // attempt to download JSON data as a string
                 try
                 {
-                    jsonData = w.DownloadString(url);
+                    /*Thread thread = new Thread(showWaitDialog);
+                    thread.SetApartmentState(ApartmentState.STA);
+                    thread.Start();
+                    wc.DownloadProgressChanged += new DownloadProgressChangedEventHandler(wc_DownloadProgressChanged);
+                    wc.DownloadDataCompleted += new DownloadDataCompletedEventHandler(wc_DownloadDataCompleted);
+                    Application.Current.Dispatcher.Invoke((Action)delegate {
+                        waitDialog = new WaitDialog();
+                        waitDialog.ShowDialog();
+
+                    });*/
+                    jsonData = wc.DownloadString(url);
+
                 }
-                catch(ArgumentNullException ane) { MessageBox.Show(ane.StackTrace, "Greska - ArgumentNullException"); }
+                catch (ArgumentNullException ane) { MessageBox.Show(ane.StackTrace, "Greska - ArgumentNullException"); }
                 catch (WebException we) { MessageBox.Show(we.StackTrace, "Greska - WebException"); }
                 catch (NotSupportedException nse) { MessageBox.Show(nse.StackTrace, "Greska - NotSupportedException"); }
                 catch (Exception e) { MessageBox.Show(e.StackTrace, "Greska - Exception"); }
 
-                // if string with JSON data is not empty, deserialize it to class and return its instance 
-                if (!string.IsNullOrEmpty(jsonData))
-                {
-                    return JsonConvert.DeserializeObject<DataFromNetwork>(jsonData);
-                }
-
-                return null;
             }
+
+            return jsonData;
         }
 
-        private void readSymbolsOfCompanies()
+        public DataFromNetwork downloadSerializedJsonDataForShares(string url)
         {
-            symbols = new Dictionary<string, string>();
+            string jsonData = downloadJsonData(url);
+            
+            // if string with JSON data is not empty, deserialize it to class and return its instance 
+            if (!string.IsNullOrEmpty(jsonData))
+            {
+                return JsonConvert.DeserializeObject<DataFromNetwork>(jsonData);
+            }
 
+            return null;
+            
+        }
+
+        public DataFromNetworkCrypto downloadSerializedJsonDataForCrypto(string url)
+        {
+            string jsonData = downloadJsonData(url);
+
+            // if string with JSON data is not empty, deserialize it to class and return its instance 
+            if (!string.IsNullOrEmpty(jsonData))
+            {
+                 return JsonConvert.DeserializeObject<DataFromNetworkCrypto>(jsonData);    
+            }
+
+            return null;
+
+        }
+
+        /*private void showWaitDialog()
+        {
+            if(waitDialog == null) { 
+                waitDialog = new WaitDialog();
+                waitDialog.ShowDialog();
+            }
+        }
+        
+        public void wc_DownloadProgressChanged(Object sender, DownloadProgressChangedEventArgs e)
+        {
+            MessageBox.Show(e.ProgressPercentage + "%", "Download");
+
+        }
+
+        public void wc_DownloadDataCompleted(Object sender, DownloadDataCompletedEventArgs e)
+        {
+            //waitDialog.Close();
+        }*/
+
+        private Dictionary<string, string> readFromFile(string path)
+        {
+            Dictionary<string, string> dict = new Dictionary<string, string>();
             string[] values;
-
-            string path = @"..\..\Files\shares.csv";
 
             using (StreamReader reader = new StreamReader(path))
             {
@@ -61,17 +124,31 @@ namespace HCI.ViewModel
                     string line = reader.ReadLine();
                     values = line.Split(',');
 
-                    symbols.Add(values[1].Trim(), values[0].Trim());
-
+                    try { 
+                    dict.Add(values[1].Trim(), values[0].Trim());
+                    }
+                    catch (Exception e) { MessageBox.Show(values[0].Trim() + " - " + values[1].Trim(), "Duplikati"); }
 
                 }
             }
+
+            return dict;
           
         }
 
-        public bool existsSymbol(string symbol)
+        public bool existsShare(string share)
         {
-            return symbols.ContainsKey(symbol);
+            return shares.ContainsKey(share);
+        }
+
+        public bool existsCrypto(string crypto)
+        {
+            return cryptos.ContainsKey(crypto);
+        }
+
+        public bool existsCurr(string curr)
+        {
+            return currs.ContainsKey(curr);
         }
     }
     
@@ -94,7 +171,7 @@ namespace HCI.ViewModel
         }
     }
 
-    public class MetaData
+    /*public class MetaData
     {
         [JsonProperty("1. Information")]
         public string Information { get; set; }
@@ -106,23 +183,49 @@ namespace HCI.ViewModel
         public string OutputSize { get; set; }
         [JsonProperty("5. Time Zone")]
         public string TimeZone { get; set; }
-    }
+    }*/
 
     public class DataFromNetwork
     {
         [JsonProperty("Meta Data")]
-        public MetaData MData { get; set; }
+        public Dictionary<string, string> MetaData { get; set; }
         [JsonProperty("Time Series (Daily)", NullValueHandling = NullValueHandling.Ignore)]
         public Dictionary<DateTime, TimeSerie> TimeSeries { get; set; }
         [JsonProperty("Weekly Time Series", NullValueHandling = NullValueHandling.Ignore)]
         public Dictionary<DateTime, TimeSerie> TimeSeriesWeekly { set { TimeSeries = value; } }
         [JsonProperty("Monthly Time Series", NullValueHandling = NullValueHandling.Ignore)]
         public Dictionary<DateTime, TimeSerie> TimeSeriesMonthly { set { TimeSeries = value; } }
+        
+        [JsonProperty("Time Series (1min)", NullValueHandling = NullValueHandling.Ignore)]
+        public Dictionary<DateTime, TimeSerie> TimeSeries1min { set { TimeSeries = value; } }
+        [JsonProperty("Time Series (5min)", NullValueHandling = NullValueHandling.Ignore)]
+        public Dictionary<DateTime, TimeSerie> TimeSeries5min { set { TimeSeries = value; } }
+        [JsonProperty("Time Series (15min)", NullValueHandling = NullValueHandling.Ignore)]
+        public Dictionary<DateTime, TimeSerie> TimeSeries15min { set { TimeSeries = value; } }
+        [JsonProperty("Time Series (30min)", NullValueHandling = NullValueHandling.Ignore)]
+        public Dictionary<DateTime, TimeSerie> TimeSeries30min { set { TimeSeries = value; } }
+        [JsonProperty("Time Series (60min)", NullValueHandling = NullValueHandling.Ignore)]
+        public Dictionary<DateTime, TimeSerie> TimeSeries60min { set { TimeSeries = value; } }
+
 
         //Ovih par redova iznad rade sledece: bice pronadjen neki od sledecih "Time Series (Daily)", "Weekly Time Series", "Monthly Time Series"
         //Koji god da bude pronadjen, njegova vrednost bice stavljena u TimeSeries
 
-        // TODO treba dodati i za intrady
+    }
+
+    public class DataFromNetworkCrypto
+    {
+        [JsonProperty("Meta Data")]
+        public Dictionary<string, string> MetaData { get; set; }
+
+        [JsonProperty("Time Series (Digital Currency Intraday)", NullValueHandling = NullValueHandling.Ignore)]
+        public Dictionary<DateTime, Dictionary<string, string>> TimeSeries { get;  set; }
+        [JsonProperty("Time Series (Digital Currency Daily)", NullValueHandling = NullValueHandling.Ignore)]
+        public Dictionary<DateTime, Dictionary<string, string>> TimeSeriesDaily { set { TimeSeries = value; } }
+        [JsonProperty("Time Series (Digital Currency Weekly)", NullValueHandling = NullValueHandling.Ignore)]
+        public Dictionary<DateTime, Dictionary<string, string>> TimeSeriesWeekly { set { TimeSeries = value; } }
+        [JsonProperty("Time Series (Digital Currency Monthly)", NullValueHandling = NullValueHandling.Ignore)]
+        public Dictionary<DateTime, Dictionary<string, string>> TimeSeriesMonthly { set { TimeSeries = value; } }
 
     }
 
